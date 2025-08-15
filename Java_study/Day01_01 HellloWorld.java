954f3d5d57724cf0b1a2d89a5740220b09fe62d1
ken <limstar9147@gmail.com>
2025-08-15 16:47:22 +0900

    Update Day01_01 HellloWorld.java

--- Java_study/Day01_01 HellloWorld.java
+++ Java_study/Day01_01 HellloWorld.java
@@ -1,3 +1,4 @@
+/* hello world */
 // ken이라는 패키지에 속한다는 선언
 package ken;
 
@@ -5,11 +6,13 @@ package ken;
 // 프로그램의 시작점 - JVM이 여기서부터 실행을 시작함
 // Main메서드가 없으면 실행이 안됨
 public class Main {
+    
     // public: 외부에서 접근 가능
     // static: 객체 생성 없이 바로 실행 가능  
     // void: 반환값 없음
     // String[] args: 프로그램 실행시 전달받는 명령어 인수들
     public static void main(String[] args) {
+        
         // 콘솔에 "Hello world!" 출력 (println = print line, 줄바꿈 포함)
         System.out.println("Hello world!");
     }
