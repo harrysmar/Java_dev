5d606ed2a4f78d1de98a927659bd2c1ba15b3f06
ken <limstar9147@gmail.com>
2025-08-15 16:58:43 +0900

    Update Day01_02 Variables.java

--- Java_study/Day01_02 Variables.java
+++ Java_study/Day01_02 Variables.java
@@ -11,21 +11,21 @@ public class Main {
    public static void main(String[] args) {
      
        /* variables */
-       // 정수형 변수
+       // [정수형 변수]
        int points;      // 변수 선언
        points = 100;    // 값 할당  
        int x = -69;     // 선언과 동시에 초기화 // 선언과 동시에 초기화는 변수를 만들면서 바로 값을 넣는 것을 말함
 
-       // 실수형 변수
+       // [실수형 변수]
        float pi = 3.14159265f;    // float는 끝에 f 필요 // float는 소수점 7자리까지 표현
        double e = 2.718281828;    // double이 더 정밀함 // double은 소수점 15자리까지 표현
 
-       // 논리형 변수
+       // [논리형 변수]
        // 참/거짓 값을 가지는 변수
        boolean likedVideo = true;   
        boolean subscribed = false;
 
-       // 문자형 변수
+       // [문자형 변수]
        // char은 '(작은따옴표) String은 "(큰따옴표)를 써야 함
        char grade = 'A';         // 단일 문자 (작은따옴표)
        String name = "Ken";      // 문자열 (큰따옴표)
